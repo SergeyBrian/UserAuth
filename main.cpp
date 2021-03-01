d69749b8f644d950cbbdcd2141cb2235ead35fed
Sergio Brianski <s.brianski@yahoo.com>
2021-03-01 21:05:30 +0300

    Use char arrays instead of string to store data

--- main.cpp
+++ main.cpp
@@ -11,21 +11,27 @@ using namespace std;
 // Global variables definition
 
 bool verbose = false;
-const string zero_hash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+const string zero_hash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4hash_length9b934ca495991b7852b855";
+const int hash_length = 64;
 
 
 // Functions definition
 
 bool isRegistered(const string& username, const string& password);
 bool checkFileHash(const string& filename, FILE * b_file, bool repair);
+bool compare(const char * a, const char * b);
 int registerUser(string username, string password);
 int login (string username, string password);
 int generateCode();
 void getParams(string * username, string * password, int argc, char ** argv);
 void debug(const string&str, bool force);
 void writeHash(FILE * file, const string& hash, int number_of_users);
+void writeHash(FILE * file, const char * hash, int number_of_users);
+void stringToArray(char * arr, const string& str);
+void equate(char * a, const char * b);
 string getFileHash(const string& filename);
 string getCurrentDateTime();
+char * stringToArray(const string& str);
 
 // Program
 
@@ -34,84 +40,137 @@ void debug(const T& str, bool force=false) {
     if (verbose||force) cout << endl << str << endl;
 }
 
-void filecopy(FILE *dest, FILE *src)
+bool compare(const char * a, const char * b) {
+    for (int i = 0; i < 64; i ++)
+        if (a[i]!=b[i]) return false;
+    return true;
+}
+
+void stringToArray(char * array, const string& str) {
+    size_t length = str.length() + 1;
+    array = new char[length];
+    strcpy(array, str.c_str());
+}
+
+char * stringToArray(const string& str) {
+    char * array;
+    size_t length = str.length() + 1;
+    array = new char[length];
+    strcpy(array, str.c_str());
+    return array;
+}
+
+void filecopy(FILE *ft, FILE *fs)
 {
-    const int size = 16384;
-    char buffer[size];
-
-    while (!feof(src))
+    char ch;
+    debug("Creating a copy of file");
+    if(fs == NULL)
     {
-        int n = fread(buffer, 1, size, src);
-        fwrite(buffer, 1, n, dest);
+        return;
     }
+    if(ft == NULL)
+    {
+        cout<<"\nError Occurred!";
+        return;
+    }
+    ch = fgetc(fs);
+    while(ch != EOF)
+    {
+        fputc(ch, ft);
+        ch = fgetc(fs);
+    }
+    fclose(fs);
+    fclose(ft);
+    debug("File copied!");
+}
 
-    fflush(dest);
+void equate(char * a, const char * b) {
+    for (int i = 0; i < 64; i ++)
+        a[i] = b[i];
 }
 
 void writeHash(FILE * file, const string& hash, int number_of_users) {
+    debug("Writing hash " + hash);
+    char * arr;
+    stringToArray(arr, hash);
     fseek(file, 0, SEEK_SET);
-    fwrite(&hash, sizeof(string), number_of_users, file);
-    fseek(file, sizeof(string) + sizeof(int), SEEK_CUR);
-    fwrite(&hash, sizeof(string), number_of_users, file);
-    fseek(file, -sizeof(string), SEEK_END);
-    fwrite(&hash, sizeof(string), number_of_users, file);
+    fwrite(arr, sizeof(char), hash_length, file);
+    fseek(file, sizeof(char)*number_of_users*hash_length + sizeof(int), SEEK_CUR);
+    fwrite(arr, sizeof(char), hash_length, file);
+    fseek(file, -hash_length*sizeof(char), SEEK_END);
+    fwrite(arr, sizeof(char), hash_length, file);
+    debug("Hash written!");
+}
+
+void writeHash(FILE * file, const char * hash, int number_of_users) {
+    fseek(file, 0, SEEK_SET);
+    fwrite(hash, sizeof(char), hash_length, file);
+    fseek(file, sizeof(char)*number_of_users*hash_length + sizeof(int), SEEK_CUR);
+    fwrite(hash, sizeof(char), hash_length, file);
+    fseek(file, -hash_length*sizeof(char), SEEK_END);
+    fwrite(hash, sizeof(char), hash_length, file);
+    debug("Hash written!");
 }
 
 bool checkFileHash(const string& filename, FILE * b_file, bool repair=false) {
-    debug("Checking file hash");
-    ifstream file(filename);
+    debug("Checking file hash for file " + filename);
     FILE * tmp = fopen(".tmp.dat", "wb+");
     int number_of_users;
-    string hash, hash1, hash2, realHash;
+    char hash[64], hash1[64], hash2[64], realHash[64];
     filecopy(tmp, b_file);
 
     fseek(tmp, 0, SEEK_SET);
-    fseek(b_file, 0, SEEK_SET);
+    fseek(b_file, sizeof(string), SEEK_SET);
 
     fread(&number_of_users, sizeof(int), 1, b_file);
-    fread(&hash, sizeof(string), 1, b_file);
-    fseek(b_file, number_of_users*sizeof(string), SEEK_CUR);
-    fread(&hash1, sizeof(string), 1, b_file);
-    fseek(b_file, -sizeof(string), SEEK_END);
-    fread(&hash2, sizeof(string), 1, b_file);
+
+    debug(number_of_users);
+
+    fread(&hash, sizeof(char), 64, b_file);
+    fseek(b_file, number_of_users*sizeof(char)*64, SEEK_CUR);
+    fread(&hash1, sizeof(char), 64, b_file);
+    fseek(b_file, -sizeof(char), SEEK_END);
+    fread(&hash2, sizeof(char), 64, b_file);
     fclose(b_file);
 
-    debug("First hash: " + hash);
-    debug("Second hash: " + hash1);
-    debug("Third hash: " + hash2);
+//    debug("First hash: ");
+//    debug("Second hash: ");
+//    debug("Third hash: ");
 
-    if (repair && (hash!=hash1||hash!=hash2||hash1!=hash2)) {
+    if (repair && !(compare(hash,hash1)||compare(hash,hash2)||compare(hash1,hash2))) {
         debug("Hash is damaged and will be repaired!");
-        if (hash != hash1 && hash1 != hash2 && hash != hash2) return false;
-        string trueHash;
-        if (hash != hash1 && hash1 == hash2) {
+        if (!(compare(hash,hash1)&&compare(hash1,hash2)&&compare(hash,hash2))) return false;
+        char trueHash[64];
+        if (compare(hash1,hash2)&&!compare(hash, hash1)) {
             // First hash is broken
-            trueHash = hash1;
+            equate(trueHash, hash1);
         }
-        if (hash1 != hash2 && hash == hash2) {
+        if (compare(hash,hash2)&&!compare(hash,hash1)) {
             // Second hash is broken
-            trueHash = hash;
+            equate(trueHash, hash);
         }
-        if (hash2 != hash && hash == hash1) {
+        if (compare(hash,hash1)&&!compare(hash,hash2)) {
             // Third hash is broken
-            trueHash = hash1;
+            equate(trueHash, hash1);
         }
         writeHash(b_file, trueHash, number_of_users);
         debug("Hash successfully repaired!");
-        realHash = trueHash;
+        equate(realHash,trueHash);
     }
 
-    fwrite(&zero_hash, sizeof(string), 1, tmp);
-    fseek(tmp, number_of_users*sizeof(string), SEEK_CUR);
-    fwrite(&zero_hash, sizeof(string), 1, tmp);
-    fseek(tmp, 0, SEEK_END);
-    fwrite(&zero_hash, sizeof(string), 1, tmp);
+//    fwrite(&zero_hash, sizeof(char), 64, tmp);
+//    fseek(tmp, number_of_users*sizeof(char)*64, SEEK_CUR);
+//    fwrite(&zero_hash, sizeof(char), 64, tmp);
+//    fseek(tmp, 0, SEEK_END);
+//    fwrite(&zero_hash, sizeof(char), 64, tmp);
 
-    realHash = getFileHash(".tmp.dat");
+    writeHash(tmp, zero_hash, number_of_users);
+
+    equate(realHash,stringToArray(getFileHash(".tmp.dat")));
     fclose(tmp);
     remove(".tmp.dat");
 
-    return (hash == realHash);
+    return (compare(hash, realHash));
 
 }
 
@@ -202,13 +261,14 @@ int registerUser(string username, string password) {
         int number_of_users = 1;
         string users[1] = {sha256(username)};
         string passwords[1] = {sha256(password)};
-        fwrite(&hashed, sizeof(string), 1, usersFile);
-        fwrite(&number_of_users, sizeof(int),  1, usersFile);
-        fwrite(users, sizeof(string), 1, usersFile);
-        fwrite(&hashed, sizeof(string), 1, usersFile);
-        fwrite(passwords, sizeof(string), 1, usersFile);
-        fwrite(&hashed, sizeof(string), 1, usersFile);
-        fclose(usersFile);
+        writeHash(usersFile, hashed, 1);
+//        fwrite(&hashed, sizeof(string), 1, usersFile);
+//        fwrite(&number_of_users, sizeof(int),  1, usersFile);
+//        fwrite(users, sizeof(string), 1, usersFile);
+//        fwrite(&hashed, sizeof(string), 1, usersFile);
+//        fwrite(passwords, sizeof(string), 1, usersFile);
+//        fwrite(&hashed, sizeof(string), 1, usersFile);
+//        fclose(usersFile);
 
         // Calculate file hash sum
         hashed = getFileHash("users.dat");
